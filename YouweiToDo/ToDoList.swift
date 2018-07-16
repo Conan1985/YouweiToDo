697d409fbc37c694575860b892c1642b2c390059
Conan1985 <Conan1985@users.noreply.github.com>
2018-07-15 22:51:24 -0400

    Slide left to remove todo list.

--- YouweiToDo/ToDoList.swift
+++ YouweiToDo/ToDoList.swift
@@ -27,4 +27,15 @@ extension ToDoList: UITableViewDataSource {
         cell.textLabel!.text = item;
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
